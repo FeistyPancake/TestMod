79750bc94259c4481e81934cae24646544744e4e
TrojanHashtag <alexlahola@yahoo.ca>
2016-05-18 22:29:26 -0700

    Added Configuration and Configuration GUI

--- src/main/java/com/trojanhashtag/testmod/TestMod.java
+++ src/main/java/com/trojanhashtag/testmod/TestMod.java
@@ -1,15 +1,16 @@
 package com.trojanhashtag.testmod;
 
-import com.trojanhashtag.testmod.configuration.ConfigurationHandler;
+import com.trojanhashtag.testmod.handler.ConfigurationHandler;
 import com.trojanhashtag.testmod.proxy.IProxy;
 import com.trojanhashtag.testmod.reference.Reference;
+import com.trojanhashtag.testmod.utility.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class TestMod
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -22,17 +23,18 @@ public class TestMod
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        LogHelper.debug("Pre Initialization Complete");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.warn("Initialization Complete");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.fatal("Post Initialization Complete");
     }
 }
---
+++ src/main/java/com/trojanhashtag/testmod/client/gui/ModGuiConfig.java
@@ -0,0 +1,22 @@
+package com.trojanhashtag.testmod.client.gui;
+
+import com.trojanhashtag.testmod.TestMod;
+import com.trojanhashtag.testmod.handler.ConfigurationHandler;
+import com.trojanhashtag.testmod.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class ModGuiConfig extends GuiConfig
+{
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
